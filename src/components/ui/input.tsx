f0f1a5dbfc071e458b9b593a69402680ffdf3377
Bayu Bona Ringgas Purba <bayubona00@mail.ugm.ac.id>
2025-09-18 09:24:37 +0000

    hi, can you try to solve the issue with this way? is it feasible? here i

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -13,7 +13,7 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<"input">>(
         )}
         ref={ref}
         {...props}
-        data-fdprocessedid-ignore={true}
+        data-fdprocessedid-ignore="true"
       />
     )
   }
